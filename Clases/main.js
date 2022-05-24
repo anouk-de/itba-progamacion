57fa51a5a9156fd42176c0e700b02db80dc95c43
anouk-de <alaferrere@itba.edu.ar>
2022-05-24 18:32:26 -0300

    clase dom 2

--- Clases/main.js
+++ Clases/main.js
@@ -41,14 +41,43 @@ function sumarEdad() {
     let edadUsuario = document.getElementById("edad").value
         document.body.append("El nombre del usuario es " + nombreUsuario + " y va a cumplir " + (parseInt(edadUsuario) + 1) )
 }
+
+function chequear() {
+    let chequeado = document.getElementById("bases").checked
+    if(chequeado != true) {
+        alert("Debe estar de acuerdo con las condiciones")
+} }
+
 //con el .value accedo al valor 
 // parseInt() y number() transforma el valor en numérico
 // al parseInt() le puedeo cambiarle la base : 0x para hexadecimal(de 16),
 // 0 para base octal(de 8), o cadena para base decimal (de 10)
 // number() es una clase que llama al tipo de dato
 
+//ECMASCRIPT 6 --> escritura de js y html en simultaneo
+//template literal 
 
+function sumar() {
+    let valor = document.getElementById("edad").value
+    return parseInt(valor) + 1
+}
 
+    let nombreUsuario = document.getElementById("nombre").value
+    let edadUsuario = document.getElementById("edad").value
+
+document.body.append(`el nombre ded usuario es ${nombreUsuario} y va a cumplir ${sumar(edadUsuario)}`)
+
+// query lo podemos para seleccionar algo 
+// query = consulta
+
+let parrafos = document.querySelectorAll("p") //puedo sellecionar por etiqueta
+console.log(parrafos)
+let seleccionarUno = document.querySelectorAll("#titutlar") //puedo seleccionar por id
+console.log(seleccionarUno)
+let seleccionarClase = document.querySelectorAll(".paises") //puedo seleccionar por clase
+console.log(seleccionarClase)
+let seleccionarClaseUnico = document.querySelector(".paises")
+console.log(seleccionarClaseUnico)
 
 
 
